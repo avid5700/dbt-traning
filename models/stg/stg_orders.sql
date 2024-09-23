77b2a4af14ce71a6de3b436d70386f9965190dd1
avid5700@gmail.com <avid5700@gmail.com>
2024-09-23 09:30:21 +0000

    developing stage orders

---
+++ models/stg/stg_orders.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select 
+    --from raw orders:
+    orderid,
+    shipmode,
+    ordersellingprice,
+    ordercostprice,
+    ordersellingprice - ordercostprice as order_profit,
+    cast(replace(shipdate,'/','-') as date)  as shipdate_dt, 
+    cast(replace(orderdate,'/','-') as date)  as orderdate_dt, 
+
+    --from raw customer:
+    c.customername,
+    c.segment, 
+    c.country,
+
+    --from raw product
+    p.category,
+    p.productname,
+    p.subcategory 
+from {{ ref('raw_orders') }} as o 
+    left join {{ ref('raw_customer') }} as c on o.customerid=c.customerid
+    left join {{ ref('raw_product') }} as p  on  o.productid = p.productid
